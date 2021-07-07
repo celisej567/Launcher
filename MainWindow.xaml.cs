0ef19cd590585136e3a97191844fd1df2ed49fc3
celisej567 <75926135+celisej567@users.noreply.github.com>
2021-07-07 14:56:09 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using SteamWorkshopLib;
+using System.Media;
+using System.IO;
 
 namespace Launcher
 {
@@ -37,11 +39,73 @@ namespace Launcher
         private void Label_MouseEnter(object sender, MouseEventArgs e)
         {
             this.startgame.Foreground = new SolidColorBrush(Color.FromArgb(255, 164, 164, 164));
+            Random rnd = new Random();
+            int sound = rnd.Next(0, 20);
+            UnmanagedMemoryStream[] rndsong =
+            {
+                Properties.Resources._1,
+                Properties.Resources._2,
+                Properties.Resources._3,
+                Properties.Resources._4,
+                Properties.Resources._5,
+                Properties.Resources._6,
+                Properties.Resources._7,
+                Properties.Resources._8,
+                Properties.Resources._9,
+                Properties.Resources._10,
+                Properties.Resources._11,
+                Properties.Resources._12,
+                Properties.Resources._13,
+                Properties.Resources._13,
+                Properties.Resources._14,
+                Properties.Resources._15,
+                Properties.Resources._17,
+                Properties.Resources._18,
+                Properties.Resources._19,
+                Properties.Resources._20,
+                Properties.Resources._21
+            };
+            SoundPlayer sp = new SoundPlayer(rndsong[sound]);
+            
+            sp.Play();
+            
+           
+
+
         }
 
         private void startgame_MouseLeave(object sender, MouseEventArgs e)
         {
             this.startgame.Foreground = new SolidColorBrush(Color.FromArgb(255, 230, 230, 230));
+        }
+
+        private void Exit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Exit.Visibility = Visibility.Hidden;
+            this.BiggerExit.Visibility = Visibility.Visible;
+        }
+
+        private void Exit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Exit.Visibility = Visibility.Visible;
+            this.BiggerExit.Visibility = Visibility.Hidden;
+        }
+
+        private void BiggerHide_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Hide_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Hide.Visibility = Visibility.Hidden;
+            this.BiggerHide.Visibility = Visibility.Visible;
+        }
+
+        private void Hide_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Hide.Visibility = Visibility.Visible;
+            this.BiggerHide.Visibility = Visibility.Hidden;
         }
     }
 }
